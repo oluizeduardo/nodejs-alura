7c254a595cf6da0e20b91a52a140d40639c4fc00
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2018-09-18 12:35:06 -0300

    Analyzing URL address.

--- productsWeb.js
+++ productsWeb.js
@@ -1,13 +1,21 @@
 // require() is a function to import everything I need.
 var http = require('http');
+var port = 3000;
+var IP = "localhost";
 
 // Creates a new server.
 var server = http.createServer(function(req,res){
-    res.end("<html><body><h1>Listing products</h1></body></html>");
+  console.log("Received a new request");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  if(req.url === "/products"){
+    res.end("<html><body><h1>Listing the products.</h1></body></html>");
+  } else {
+    res.end("<html><body><h1>Homepage.</h1></body></html>");
+  }
 });
 
-// sets the port.
-server.listen(3000);
+// sets the listener.
+server.listen(port, IP);
 
 // Show the message server is running.
-console.log("Server is Running!");
+console.log("Server is Running at http://"+IP+":"+port+"/");
